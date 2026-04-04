c127cf6b2b46e00f41940b629aca8abc7ca7df81
hka27015 <davtyansaro@yandex.ru>
2026-04-04 18:17:53 +0300

    new function

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from cats.views import cat_list
+from cats.views import (
+    CatViewSet,
+    UserViewSet,
+    AchievementViewSet
+)
+
+router = DefaultRouter()
+router.register('cats', CatViewSet)
+router.register('users', UserViewSet)
+router.register('achievements', AchievementViewSet)
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
-
-
