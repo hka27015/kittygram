ab344377115e73b9012b4ef3fe43fb4eb758680b
hka27015 <davtyansaro@yandex.ru>
2026-06-08 19:49:36 +0300

    fix filter

--- cat_of_day/views.py
+++ cat_of_day/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from django.utils.timezone import now
 from django.db.models import Count
 from datetime import timedelta
+from django_filters.rest_framework import DjangoFilterBackend
 
 from cats.models import Cat
 from .models import DailyCat, CatVote
@@ -14,6 +15,8 @@ from .serializers import CatVoteSerializer, DailyCatSerializer
 class DailyCatViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = DailyCat.objects.all().order_by('-date')
     serializer_class = DailyCatSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['date']
 
 
     @action(detail=False, methods=['get'])
